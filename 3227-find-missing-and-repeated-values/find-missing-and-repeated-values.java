9d401f6efb1f9b6d725b7acbbe7fd7e23fed9f33
aitipamulalassya <161203075+aitipamulalassya@users.noreply.github.com>
2025-03-06 18:54:40 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (62.19%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -6,22 +6,16 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                int val=grid[i][j];
-               System.out.println(val);
+               //System.out.println(val);
                if(a[val]==0){
                 a[val]=1;
                }else{
                 ans[0]=val;
-                
-               }
-            }
-            
-        }
+                 } } }
         for(int i=1;i<=n*n;i++){
             if(a[i]==0){
                 ans[1]=i;
                 break;
-            }
-        }
-        return ans;
-    }
+            } }
+        return ans; }
 }
